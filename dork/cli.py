5c9be98e4abda49747771ca89f441e8beea2d3ba
Jon Watson <jon@neighbrum.com>
2019-06-10 20:09:13 -0600

    added cli before entering game

--- dork/cli.py
+++ dork/cli.py
@@ -1,15 +1,74 @@
 # -*- coding: utf-8 -*-
 """Basic CLI Dork.
 """
+import shlex
 
-__all__ = ["main"]
+import argparse
+import os
+import sys
+import time
+import cursor
+
+__all__ = ["in"]
+
+VERSION = "0.0.1"
+
+def the_predork_cli(*args):
+    """ command line interface supports -l -i -o -v """
+    global VERSION
+    version = VERSION
+    parser = argparse.ArgumentParser(description='Dork command line interface')
+
+    parser.add_argument('-l', '--list', action='store_true', help='list available mazes')
+    parser.add_argument('-i', '--init', help='-i <mazename> initializes dork with mazename')
+    parser.add_argument('-o', '--out', help='-o <mazename> generates a maze and saves it')
+    parser.add_argument('-v', '--version', action='store_true', help="prints version and exits")
+
+    arglist = parser.parse_args(args[1:])
+
+    run_flag = False
+
+    if arglist.out:
+        f = open("mazes/"+arglist.out+".drk", "w")
+        cursor.hide()
+        time.sleep(0.5)
+        dots = ("Generating maze    ", "Generating maze .", "Generating maze ..", "Generating maze ...")
+        for t in range(20):
+            print("{}".format(dots[t%4]), end="\r")
+            time.sleep(1)
+        print("{}".format(" "*len(dots[-1])))
+        cursor.show()
+        print("Done, maze \""+arglist.out+"\" saved")
+        f.close()
+        run_flag = True
+
+    if arglist.version:
+        print("Dork version --> " + version)
+        run_flag = True
+    
+    if arglist.list or arglist.init:
+        mazes = []
+        for (dirpath, dirnames, filenames) in os.walk("mazes/"):
+            mazes.extend(filenames)
+        only_maze_files = [maze for maze in mazes if maze.find(".drk") > 0]
+        if arglist.list:
+            print(os.linesep.join(only_maze_files))
+            run_flag = True
+        if arglist.init:
+            arglist.init = arglist.init + ".drk"
+            if arglist.init in only_maze_files:
+                print("loaded maze "+arglist.init)
+            else:
+                print("maze "+arglist.init+" does not exist")
+            run_flag = False
+
+    return run_flag
+        
 
 
 def main(*args):
     """Main CLI runner for Dork
     """
-    script_name = args[0] if args else '???'
-    if "-h" in args or '--help' in args:
-        print("usage:", script_name, "[-h]")
-    else:
-        print(*args)
+    
+    if not the_predork_cli(*args):
+        print("running dork")
