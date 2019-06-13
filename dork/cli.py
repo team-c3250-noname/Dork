862b4afe063fdb169d54e15919b1f6e76f0b7ee8
Jorgesilva15 <jsilva36@msudenver.edu>
2019-06-13 11:49:42 -0600

    interaction code

--- dork/cli.py
+++ dork/cli.py
@@ -13,3 +13,33 @@ def main(*args):
         print("usage:", script_name, "[-h]")
     else:
         print(*args)
+
+#interaction
+
+def prompt()
+    print("\n" + "What would you like to do?")
+    action = input("> ")
+    acceptable_actions = ['move', 'go', 'walk', 'travel', 'quit', 'examine', 'inspect', 'look']
+    while action.lower() not in acceptable_actions:
+        print ("Unknown action, try again.\n")
+        if action.lower == 'quit':
+            sys.exit()
+        elif action.lower() in ['move', 'go', 'travel', 'walk']
+            player_move(action.lower())
+        elif action.lower() in ['examine', 'inspect', 'interact', 'look', 'pick up']
+            player_examine(action.lower())
+
+def player_move(action)
+    if action.lower() in ['up', 'north']
+        print("This will take you north")
+    elif action.lower() in ['down', 'south']
+        print('This will take you south')
+    elif action.lower() in ['left', 'west']
+        print('This will take you west')
+    elif action.lower() in ['right', 'east']
+        print('This will take you east')
+
+def player_examine(action)
+    if action.lower() in ['examine', 'inspect']
+    
+    print()
