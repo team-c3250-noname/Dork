0e2dd8a55b674c6fa84d40d621cd2e0db3608bf2
Matthew Sorge <msorge1@msudenver.edu>
2019-06-16 16:54:22 -0600

    Renaming yaml.py due to it causing an issue when importing yaml

--- dork/saveload.py
+++ dork/saveload.py
@@ -1,10 +1,10 @@
-import yaml
 from pprint import pprint
+import yaml
 
 ITEMS = ["Key to Armory", "Sword", "Torch", "Gold"]
 DIRECTIONS = ["north", "south", "east", "west"]
 
-def _load(file_name = "./yaml/dork.yml"):
+def _load(file_name = "./dork/yaml/dork.yml"):
     with open(file_name) as file:
         data = yaml.safe_load(file.read())
 
@@ -12,6 +12,7 @@ def _load(file_name = "./yaml/dork.yml"):
 
 def _save():
     # Decide how to format save data
+    # For now we have a test dork.yml file
     return
 
 def _path(rooms, name, direction):
@@ -26,24 +27,49 @@ def _path(rooms, name, direction):
         other = room[direction]
         print(f"{other} is {direction} of {name}.")
 
+def _item(rooms, name, item):
+    room = rooms[name]
+    if item not in room:
+        print(f"{name} does not have {item} in it.")
+    elif room[item] is None:
+        print(f"There are no items in {name}.")
+    elif room[item] not in rooms:
+        print(f"{item} in {name} will lead to an error.")
+    else:
+        other = room[item]
+        print(f"{other} is an item in {name}.")
+
 def main():
     data = _load()
     print("Data that was loaded:")
     pprint(data)
 
-    print("Checking room list...")
+    print("Checking room and item list...")
     if "Rooms" not in data:
         print("No rooms found.")
+        return
+
+    if "Items" not in data:
+        print("No items found.")
         return
 
     if not isinstance(data["Rooms"], dict):
         print("Rooms in data were not proper data.")
         return
 
+    if not isinstance(data["Items"], dict):
+        print("Items in data were not proper data.")
+        return
+
     rooms = data["Rooms"]
     for name in rooms:
         for direction in DIRECTIONS:
             _path(rooms, name, direction)
+
+    items = data["Items"]
+    for name in items:
+        for item in ITEMS:
+            _item(rooms, name, item)
 
 if __name__ == "__main__":
     main()
