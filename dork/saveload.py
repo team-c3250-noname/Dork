232fe4dec8e3f99091e20c124102d0932465d672
Matthew Sorge <msorge1@msudenver.edu>
2019-06-16 17:30:17 -0600

    Current working version

--- dork/saveload.py
+++ dork/saveload.py
@@ -1,7 +1,9 @@
+# Generously inspired by our LA,
+# https://github.com/LSmith-Zenoscave
+
 from pprint import pprint
 import yaml
 
-ITEMS = ["Key to Armory", "Sword", "Torch", "Gold"]
 DIRECTIONS = ["north", "south", "east", "west"]
 
 def _load(file_name = "./dork/yaml/dork.yml"):
@@ -27,18 +29,6 @@ def _path(rooms, name, direction):
         other = room[direction]
         print(f"{other} is {direction} of {name}.")
 
-def _item(rooms, name, item):
-    room = rooms[name]
-    if item not in room:
-        print(f"{name} does not have {item} in it.")
-    elif room[item] is None:
-        print(f"There are no items in {name}.")
-    elif room[item] not in rooms:
-        print(f"{item} in {name} will lead to an error.")
-    else:
-        other = room[item]
-        print(f"{other} is an item in {name}.")
-
 def main():
     data = _load()
     print("Data that was loaded:")
@@ -49,27 +39,14 @@ def main():
         print("No rooms found.")
         return
 
-    if "Items" not in data:
-        print("No items found.")
-        return
-
     if not isinstance(data["Rooms"], dict):
         print("Rooms in data were not proper data.")
-        return
-
-    if not isinstance(data["Items"], dict):
-        print("Items in data were not proper data.")
         return
 
     rooms = data["Rooms"]
     for name in rooms:
         for direction in DIRECTIONS:
             _path(rooms, name, direction)
-
-    items = data["Items"]
-    for name in items:
-        for item in ITEMS:
-            _item(rooms, name, item)
 
 if __name__ == "__main__":
     main()
