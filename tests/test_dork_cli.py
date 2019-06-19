fd725af8fdbc658918e614ef2aa46b2c6ca64a21
Jorgesilva15 <jsilva36@msudenver.edu>
2019-06-19 09:56:36 -0600

    added tests

--- tests/test_dork_cli.py
+++ tests/test_dork_cli.py
@@ -12,7 +12,9 @@ def test_cli_exists(run):
     assert isinstance(dork.cli.main, FunctionType)
     try:
         run(dork.cli.main)
+        run(dork.cli.main, input_values=['jump', 'quit'])
         run(dork.cli.main, input_values=['play', 'quit'])
+        run(dork.cli.main, input_values=['play', 'jump high', 'quit'])
         run(dork.cli.main, input_values=['play', 'move north', 'quit'])
         run(dork.cli.main, input_values=['play', 'move south', 'quit'])
         run(dork.cli.main, input_values=['play', 'move west', 'quit'])
