3f0ee2bae12abaec0b21087ac085d4730d719129
Jeffery Walto <jwalto@msudenver.edu>
2019-07-17 13:31:02 -0600

    test saveload

--- tests/test_dork_saveload.py
+++ tests/test_dork_saveload.py
@@ -16,8 +16,8 @@ def testsave(run):
             # Should not call load directly
             data = yaml.safe_load(file.read())
         game = types.Game(data)
-        run(dork.saveload.save, game, input_values=['default'])
-        run(dork.saveload.save, game, input_values=['\0', 'default'])
+        run(dork.saveload.save, game, input_values=['roomdatatest'])
+        run(dork.saveload.save, game, input_values=['\0', 'roomdatatest'])
     except:  # noqa: E722
         raise AssertionError("cannot run 'dork' command")
 
