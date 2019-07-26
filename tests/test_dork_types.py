ed60861e23d5d2f8c545136c87444961b41d5947
Jon Watson <jon@neighbrum.com>
2019-07-25 21:45:22 -0600

    added tests

--- tests/test_dork_types.py
+++ tests/test_dork_types.py
@@ -1,7 +1,7 @@
 """tests types not covered in other test files
 """
 import yaml
-from dork.types import Map, Game
+from dork.types import Map, Game, Room
 
 
 def dork_test_map():
@@ -9,5 +9,17 @@ def dork_test_map():
     """
     with open('./dork/yaml/default.yml') as file:
         data = yaml.safe_load(file.read())
-    minimap = Map(Game(data))
+
+    room = {room_name: Room(room) for room_name,
+            room in data.get('rooms').items()}
+
+    game = Game(data)
+    minimap = Map(game)
+    assert all([room_name in minimap.origins for room_name in room]),\
+        "atleast one room was not assigned an origin"
+    assert all([room_name in minimap.map for room_name in room]),\
+        "atleast one room name was not found in the map"
+    assert all([node_info["edges"] for _, node_info in minimap.map.items()]),\
+        "room map is not fully connected"
+    
     minimap.update()
